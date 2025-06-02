985b29aa844f4b1857aeefeb18082663a2b1e98d
Benedikt Zundel <benedikt.zundel@live.com>
2025-06-02 23:43:25 +0200

    backend(feat): include more data in query

--- app/src/main/java/de/frauas/weather_flosscast/DataHelper.kt
+++ app/src/main/java/de/frauas/weather_flosscast/DataHelper.kt
@@ -15,7 +15,7 @@ private fun stripCoordinate(value: Double): String {
 }
 
 private fun buildUrl(latitude: Double, longitude: Double): String {
-    return "https://api.open-meteo.com/v1/forecast?latitude=${stripCoordinate(latitude)}&longitude=${stripCoordinate(longitude)}&hourly=temperature_2m"
+    return "https://api.open-meteo.com/v1/forecast?latitude=${stripCoordinate(latitude)}&longitude=${stripCoordinate(longitude)}&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,weather_code,rain,showers,snowfall"
 }
 
 private fun getRawWeatherData(requestUrl: String): String {
@@ -41,18 +41,56 @@ private fun convertToForecastObject(body: String): Forecast {
     // FIXME wrap in try catch for NullPointerException (non-null assertion (!!.))
     val json: JsonObject = Json.parseToJsonElement(body).jsonObject
 
-    val unit: String = json["hourly_units"]!!.jsonObject["temperature_2m"]!!.jsonPrimitive.content
+    // extract units of hourly data
+    val unitsJson: JsonObject = json["hourly_units"]!!.jsonObject
+    val units = Units(
+        unitsJson["temperature_2m"]!!.jsonPrimitive.content,
+        unitsJson["relative_humidity_2m"]!!.jsonPrimitive.content,
+        unitsJson["precipitation_probability"]!!.jsonPrimitive.content,
+        unitsJson["rain"]!!.jsonPrimitive.content,
+        unitsJson["showers"]!!.jsonPrimitive.content,
+        unitsJson["snowfall"]!!.jsonPrimitive.content
+    )
 
+    // extract hourly values and cast to respective type
     val hourly: JsonObject = json["hourly"]!!.jsonObject
     val hourlyTime = hourly["time"]!!.jsonArray.map { LocalDateTime.parse(it.jsonPrimitive.content) }
     val hourlyTemperature = hourly["temperature_2m"]!!.jsonArray.map { it.jsonPrimitive.content.toDouble() }
+    val hourlyRelativeHumidity = hourly["relative_humidity_2m"]!!.jsonArray.map { it.jsonPrimitive.content.toInt() }
+    val hourlyPrecipitationProbability = hourly["precipitation_probability"]!!.jsonArray.map { it.jsonPrimitive.content.toInt() }
+    val hourlyRain = hourly["rain"]!!.jsonArray.map { it.jsonPrimitive.content.toDouble() }
+    val hourlyShowers = hourly["showers"]!!.jsonArray.map { it.jsonPrimitive.content.toDouble() }
+    val hourlySnowfall = hourly["snowfall"]!!.jsonArray.map { it.jsonPrimitive.content.toDouble() }
+    val hourlyWeatherCode = hourly["weather_code"]!!.jsonArray.map { it.jsonPrimitive.content.toInt() }
 
-    assert(hourlyTime.size == hourlyTemperature.size)
+    // assert that all extracted lists have the same length
+    assert(listOf
+        (hourlyTime
+        , hourlyTemperature
+        , hourlyRelativeHumidity
+        , hourlyPrecipitationProbability
+        , hourlyRain
+        , hourlyShowers
+        , hourlySnowfall
+        , hourlyWeatherCode
+        ).map { it.size }.toSet().size == 1)
 
-    val temperatures: List<TemperatureItem> = hourlyTime.zip(hourlyTemperature).map { (time, temperature) -> TemperatureItem(time, temperature) }
-    val forecasts: List<DailyForecast> = temperatures.groupBy { it.dateTime.date }.map { (date, values) -> DailyForecast(date, values) }
+    val hourlyValues: List<Hourly> = hourlyTime.indices.map {
+        Hourly(
+            dateTime = hourlyTime[it],
+            temperature = hourlyTemperature[it],
+            relativeHumidity = hourlyRelativeHumidity[it],
+            precipitationProbability = hourlyPrecipitationProbability[it],
+            rain = hourlyRain[it],
+            showers = hourlyShowers[it],
+            snowfall = hourlySnowfall[it],
+            weatherCode = hourlyWeatherCode[it]
+        )
+    }
 
-    return Forecast(forecasts, unit)
+    val dailyForecasts: List<DailyForecast> = hourlyValues.groupBy { it.dateTime.date }.map { (date, measurements) -> DailyForecast(date, measurements) }
+
+    return Forecast(dailyForecasts, units)
 }
 
 fun getForecast(latitude: Double, longitude: Double): Forecast {
--- app/src/main/java/de/frauas/weather_flosscast/Model.kt
+++ app/src/main/java/de/frauas/weather_flosscast/Model.kt
@@ -5,16 +5,30 @@ import kotlinx.datetime.LocalDateTime
 
 data class Forecast(
     val days: List<DailyForecast>,
-    val unit: String
+    val units: Units
 )
 
 data class DailyForecast(
     val date: LocalDate,
-    val temperatures: List<TemperatureItem>
+    val hourlyValues: List<Hourly>
 )
 
-data class TemperatureItem(
+data class Hourly(
     val dateTime: LocalDateTime,
-    val temperature: Double
+    val temperature: Double,
+    val relativeHumidity: Int,
+    val precipitationProbability: Int,
+    val rain: Double,
+    val showers: Double,
+    val snowfall: Double,
+    val weatherCode: Int
 )
 
+data class Units(
+    val temperature: String,
+    val humidity: String,
+    val precipitationProbability: String,
+    val rain: String,
+    val showers: String,
+    val snow: String,
+)
