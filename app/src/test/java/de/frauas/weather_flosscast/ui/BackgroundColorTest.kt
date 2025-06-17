93e0235cca1d43dd395b7eda2f53890bf86ef1cd
dominikVibox <90802318+sgedodo@users.noreply.github.com>
2025-06-17 23:37:10 +0200

    TESTING: Added BackgroundColorTest.kt These are Unittests which test if the right background color gets returned

---
+++ app/src/test/java/de/frauas/weather_flosscast/ui/BackgroundColorTest.kt
@@ -0,0 +1,47 @@
+package de.frauas.weather_flosscast.ui
+
+import kotlin.random.Random
+import androidx.compose.ui.graphics.Color
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class BackgroundColorTest {
+    @Test
+    fun nightColorTest(){
+        val backgroundColor = colorForWmoCode(Random.nextInt(0,101), true)
+        assertEquals(Color(0xFF37474F), backgroundColor)
+    }
+
+    @Test
+    fun dayClearSkyColor(){
+        val backgroundColor = colorForWmoCode(0, false)
+        assertEquals(Color(0xFF33AAFF), backgroundColor)
+    }
+
+    @Test
+    fun rainOrStormColor(){
+        val backgroundColor = colorForWmoCode(Random.nextInt(1,19), false)
+        val backgroundColor2 = colorForWmoCode(Random.nextInt(30,50), false)
+        val backgroundColor3 = colorForWmoCode(Random.nextInt(70,80), false)
+        assertEquals(Color(0xFF33AAFF), backgroundColor)
+        assertEquals(Color(0xFF33AAFF), backgroundColor2)
+        assertEquals(Color(0xFF33AAFF), backgroundColor3)
+    }
+
+    @Test
+    fun snowOrCloudsColor(){
+        val backgroundColor = colorForWmoCode(Random.nextInt(20,30), false)
+        val backgroundColor2 = colorForWmoCode(Random.nextInt(50,70), false)
+        val backgroundColor3 = colorForWmoCode(Random.nextInt(80,100), false)
+        assertEquals(Color(0xFF808080), backgroundColor)
+        assertEquals(Color(0xFF808080), backgroundColor2)
+        assertEquals(Color(0xFF808080), backgroundColor3)
+    }
+
+    @Test
+    fun backupColor(){
+        val backgroundColor = colorForWmoCode(Random.nextInt(100,10000), false)
+        assertEquals(Color(0xFFB0BEC5), backgroundColor)
+    }
+}
